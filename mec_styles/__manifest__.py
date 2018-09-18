3e06d2fc3ef546b58c4f7b4215c57e9158410862
cesar.colli <cesar.colli@bsdenterprise.com>
2018-09-18 02:06:46 -0500

    [NEW] add styles

---
+++ mec_styles/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © <2018> <IDEANET (cesarcolli@ideanet.com.mx)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'MEC Styles',
+    'version': '1.0.7',
+    'category': 'MEC',
+    'description': """
+MEC Styles
+====================================================
+    """,
+    'depends': ['web_enterprise'],
+    'data': [
+        "views/webclient_templates.xml",
+    ],
+    'demo': [],
+    'installable': True
+}
